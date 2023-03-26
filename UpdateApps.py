d3b9ca83b7d27eb74985e7d5a58bdd8e556cd3d9
Jason Nordmann <thenxproject@gmail.com>
2023-03-26 18:48:36 -0500

    Adding comments.

--- UpdateApps.py
+++ UpdateApps.py
@@ -3,6 +3,8 @@ import os
 
 def update_apps_debian():
     """Update app installed through apt on Debian."""
+
+    # Runs a series of commands as sudo to install updates.
     os.system("sudo apt update")
     os.system("sudo apt upgrade")
     os.system("sudo apt dist-upgrade")
@@ -12,17 +14,23 @@ def update_apps_debian():
 
 def update_apps_ubuntu():
     """Updates apps installed through snap and apt on Ubuntu."""
+
+    # Installs apt package updates.
     update_apps_debian()
+    # Installs snap updates.
     os.system("sudo snap refresh")
 
 
 def update_apps_mac_os_x():
     """Updates aps installed through Homebrew."""
+
+    # Runs Homebrew apt updates.
     os.system("brew update")
     os.system("brew upgrade")
 
 
 if __name__ == '__main__':
+    # Detects OS and runs updates for matches.
     if os.uname().sysname == "Linux":
         if "Ubuntu" in os.uname().version:
             update_apps_ubuntu()
