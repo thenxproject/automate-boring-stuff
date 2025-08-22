a4263b561767b34f5dc5f09259dc61982ec18fe9
Jason Nordmann <thenxproject@gmail.com>
2025-08-22 17:52:22 -0500

    Adding command to convert media files using HandBrakeCLI

---
+++ ConvertMedia.py
@@ -0,0 +1,40 @@
+import os
+
+
+def get_files_in_directory(path: str) -> list[str]:
+    return [file for file in os.listdir(os.path.expanduser(path)) if file.endswith(".mkv")]
+
+
+def get_sub_directories(path: str) -> list[str]:
+    return [folder for folder in os.listdir(os.path.expanduser(path)) if not folder.startswith(".")]
+
+
+
+def convert_file(path: str, file_name: str) -> None:
+    os.system(f"""HandBrakeCLI -i "{path}/{file_name}" -o "/mnt/Temp/Converted/{file_name.replace(".mkv", ".mp4")}" --preset "HQ 1080p30 Surround" -v 0""")
+
+
+def convert_movies() -> None:
+    letters = get_sub_directories("/mnt/Movies2")
+    for letter in letters:
+        movies = get_sub_directories(f"/mnt/Movies2/{letter}")
+        for movie in movies:
+            files = get_files_in_directory(f"/mnt/Movies2/{letter}/{movie}")
+            for file in files:
+                convert_file(f"/mnt/Movies2/{letter}/{movie}", file)
+
+
+def convert_tv() -> None:
+    shows = get_sub_directories("/mnt/TV2")
+    for show in shows:
+        seasons = get_sub_directories(f"/mnt/TV2/{show}")
+        for season in seasons:
+            episodes = get_files_in_directory(f"/mnt/TV2/{show}/{season}")
+            for episode in episodes:
+                convert_file(f"/mnt/TV2/{show}/{season}", episode)
+
+
+# Entrypoint for the running of the application
+if __name__ == '__main__':
+    convert_movies()
+    convert_tv()
